59fb0fc8f2dce4bb647040f19e6cc724674dad4e
StefanosTouf <stefetoufe@gmail.com>
2021-12-22 23:08:08 +0000

    Parse syslog to hashmap

--- src/clojure/relaggregator/core.clj
+++ src/clojure/relaggregator/core.clj
@@ -2,10 +2,14 @@
   (:require
     [clojure.core.async
      :as a
-     :refer [>! <! go chan]])
+     :refer [>! <! go chan]]
+    [clojure.string :as s])
   (:import
     (relaggregator.LogServer
       LogServer)))
+
+
+(set! *warn-on-reflection* true)
 
 
 (defn router-maker
@@ -13,15 +17,53 @@
   (fn [msg] (doseq [ch channels] (go (>! ch msg)))))
 
 
-(defn metrics-processor []
+(defn make-syslog-record
+  [priority version timestamp hostname app-name procid msgid structured-data message]
+  {:header {:priority priority
+            :version version
+            :timestamp timestamp
+            :hostname hostname
+            :app-name app-name
+            :process-id procid
+            :message-id msgid}
+   :structured-data structured-data
+   :message message})
+
+
+(defn parse-structured-data
+  [sd-str]
+  (let [[id & params] (s/split sd-str #" |=")]
+    (apply hash-map
+           (cons :sd-id
+                 (cons id (loop [[k v & params] params acc ()]
+                            (if-not v acc
+                                    (recur params (cons (keyword k) (cons v acc))))))))))
+
+
+(defn syslog-to-record
+  [log]
+  (let
+    [[pri-v ts hn an pid msgid sd-msg] (s/split log #" " 7)
+     [pri v] (rest (s/split pri-v #"<|>"))
+     [str-d-bracket msg] (s/split sd-msg #"- |] ")
+     str-d (parse-structured-data
+             (s/replace str-d-bracket #"^\[|]$" ""))]
+    (make-syslog-record pri v ts hn an pid msgid str-d msg)))
+
+
+(defn metrics-processor
+  []
   (let [in (chan)]
     (go (while true (println (str (<! in) "--metrics"))))
     in))
 
-(defn main-processor []
+
+(defn main-processor
+  []
   (let [in (chan)]
-    (go (while true (println (str (<! in) "--main"))))
+    (go (while true (println (syslog-to-record (<! in)))))
     in))
+
 
 (defn -main
   [& _args]
