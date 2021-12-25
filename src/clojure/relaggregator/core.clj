59bfbecfb7b70888d8437501d1fcc5b08c8d48e9
StefanosTouf <stefetoufe@gmail.com>
2021-12-25 11:09:52 +0000

    store accumulated logs when restarting

--- src/clojure/relaggregator/core.clj
+++ src/clojure/relaggregator/core.clj
@@ -11,41 +11,23 @@
       LogServer)))
 
 
-(defn metrics-processor
-  []
-  (let [in (chan (buffer 1))]
-    (go (while true (str (<! in) "--metrics")))
-    in))
-
-
-(defn printer
-  []
-  (let [buff-size (conf/config :logs-per-write)
-        in        (chan (buffer buff-size))]
-    (go
-      (loop [inserts []]
-        (if (= buff-size (count inserts))
-          (do (db/insert inserts) (recur [(<! in)]))
-          (recur  (conj inserts (<! in))))))
-    in))
-
-
 (defn -main
   [& _args]
-  (let [retries      (conf/config :null-retries)
+  (let [retries      20
         port         (conf/config :port)
-        metrics-ch   (metrics-processor)
-        main-ch      (printer)
+        metrics-ch   (p/metrics-processor)
+        main-ch      (p/printer)
         logserver    (new LogServer port)
         reader       (.getReader logserver)]
     (loop [msg (.readLine reader) nils 0]
       (cond
         (> nils retries) (do (println "Too many nulls, restarting...")
+                             (>!! main-ch :process/shutdown)
                              (.close logserver)
                              (-main))
         msg              (do (go (->> msg p/syslog-to-record (>! main-ch)))
                              (go (->> msg p/syslog-to-record (>! metrics-ch)))
-                             (recur (.readLine reader) nils))
+                             (recur (.readLine reader) 0))
         :else            (recur (.readLine reader) (+ nils 1))))))
 
 
--- src/clojure/relaggregator/process.clj
+++ src/clojure/relaggregator/process.clj
@@ -1,6 +1,12 @@
 (ns relaggregator.process
   (:require
-    [clojure.string :as s])
+    [clojure.core.async
+     :as a
+     :refer [>! <! go chan pipeline >!! <!! buffer]]
+    [clojure.string :as s]
+    [relaggregator.config :as conf]
+    [relaggregator.database :as db]
+    )
   (:import
     (java.sql
       Timestamp)
@@ -44,3 +50,22 @@
      (type-parser id str-d)
      (type-parser id msg)]))
 
+
+(defn metrics-processor
+  []
+  (let [in (chan (buffer 1))]
+    (go (while true (str (<! in) "--metrics")))
+    in))
+
+
+(defn printer
+  []
+  (let [buff-size (conf/config :logs-per-write)
+        in        (chan (buffer buff-size))]
+    (go
+      (loop [inserts []]
+        (let [incoming (<! in)]
+          (if (or (= incoming :process/shutdown) (= buff-size (count inserts)))
+            (do (db/insert inserts) (recur [incoming]))
+            (recur  (conj inserts incoming))))))
+    in))
