fff1e1f1fdd7342c532a6d7193f5ab8329d38f86
StefanosTouf <stefetoufe@gmail.com>
2021-12-28 23:25:58 +0000

    All operators

--- src/clojure/relaggregator/filter.clj
+++ src/clojure/relaggregator/filter.clj
@@ -6,15 +6,16 @@
 
 ;; filters:
 ;;  combinators:
-;;     - or
-;;     - and
-;;     - not
 ;;  general:
 ;;     - eq
+;;     - one_of
+;;     - not_eq
+;;     - not_one_of
 ;;  numbers:
 ;;     - gt   -needs to transform log into num if not already (eg coming from regex of message)
 ;;     - lt   -needs to transform log into num if not already (eg coming from regex of message)
-;;   
+;;     - gte
+;;     - lte
 ;;
 ;; example
 ;; {
@@ -35,16 +36,36 @@
 
 
 (def filters
-  {:filters {:event {:eq "Error"}}})
+  {:filters
+   {:priority
+    {:or {:gt 1000 :eq 1000}}}})
 
 
 (def operators
-  {:eq (fn [filter-val message-val]
+  {:eq (fn [filter-val log-val]
          (if (number? filter-val)
-           (= filter-val (read-string message-val))
-           (= filter-val message-val)))
-   :gt #(> %1 (read-string %2))
-   :lt #(< %1 (read-string %2))})
+           (= filter-val (read-string log-val))
+           (= filter-val log-val)))
+   :not_eq (fn [filter-val log-val]
+             (not (if (number? filter-val)
+                    (= filter-val (read-string log-val))
+                    (= filter-val log-val))))
+   ;; gt and lt make more sense as their opposites 
+   ;; when considering how the filters are written
+   :gte (fn [filter-val log-val]
+          (let [log-v (if (string? log-val) (read-string log-val) log-val)]
+            (<= filter-val log-v)))
+   :lte (fn [filter-val log-val]
+          (let [log-v (if (string? log-val) (read-string log-val) log-val)]
+            (>= filter-val log-v)))
+   :gt (fn [filter-val log-val]
+         (let [log-v (if (string? log-val) (read-string log-val) log-val)]
+           (< filter-val log-v)))
+   :lt (fn [filter-val log-val]
+         (let [log-v (if (string? log-val) (read-string log-val) log-val)]
+           (> filter-val log-v)))
+   :one_of (fn [filter-set log-val] (boolean (filter-set log-val)))
+   :not_one_of (fn [filter-set log-val] (not (boolean (filter-set log-val))))})
 
 
 (defn filters-to-pred-vec
@@ -52,8 +73,8 @@
   (map
     (fn [[field-name predicate-map]]
       (let [[op-k] (keys predicate-map)
-            op     (operators op-k)
-            [v]    (vals predicate-map)]
+            [v]    (vals predicate-map)
+            op     (operators op-k)]
         [field-name #(op v %)]))
     filters))
 
@@ -62,13 +83,13 @@
   [pred-vec syslog-record]
   (reduce
     (fn [acc [field-key predicate]]
-      (let [test-val (field-key syslog-record)
-            _         (println (predicate test-val))]
+      (let [test-val (field-key syslog-record)]
         (and acc (predicate test-val))))
     true pred-vec))
 
 
-(def sample-log "<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 [exampleSDID@32473 iut=\"3\" eventSource=\"Application\" eventID=\"1011\"] Erro: application event log entry...")
+(def sample-log
+  "<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 [exampleSDID@32473 iut=\"3\" eventSource=\"Application\" eventID=\"1011\"] 13: application event log entry...")
 
 
 (should-pass? (filters-to-pred-vec filters) (to-db-pipeline sample-log))
